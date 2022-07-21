29b02777d9eb545d875236af7273fbe3ed13e703
unknown <priyansh@innoppl.com>
2022-07-21 17:16:39 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -241,3 +241,20 @@ $(document).ready(function(){
   });
 });
 
+$('[data-fancybox]').fancybox({
+  // Options will go here
+  buttons : [
+    'close'
+  ],
+  wheel : false,
+  transitionEffect: "slide",
+   // thumbs          : false,
+  // hash            : false,
+  loop            : true,
+  // keyboard        : true,
+  toolbar         : false,
+  // animationEffect : false,
+  // arrows          : true,
+  clickContent    : false
+});
+
